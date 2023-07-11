d2588c84e976ffbfe08b7ab401b2d7686fb7dbbf
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-07-11 17:54:13 +0100

    implemented authorisation with private-route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,9 @@ import { ModalProvider } from '@/components/ui/modal/modal.context';
 import ManagedModal from '@/components/ui/modal/managed-modal';
 import { CartProvider } from '@/store/quick-cart/cart.context';
 import ManagedDrawer from '@/components/ui/drawer/managed-drawer';
+import DefaultSeo from '@/components/seo/default-seo';
+import PrivateRoute from '@/lib/private-route';
+import { ToastContainer } from 'react-toastify';
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -18,6 +21,8 @@ type AppPropsWithLayout = AppProps & {
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const authenticationRequired = Component.authenticationRequired ?? false;
+
   const { locale } = useRouter();
   const dir = getDirection(locale);
 
@@ -28,10 +33,18 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
           <ModalProvider>
             <CartProvider>
               <>
-                {getLayout(<Component {...pageProps} />)}
+                <DefaultSeo />
+                {authenticationRequired ? (
+                  <PrivateRoute>
+                    {getLayout(<Component {...pageProps} />)}
+                  </PrivateRoute>
+                ) : (
+                  getLayout(<Component {...pageProps} />)
+                )}
+                <ManagedModal />
+                <ManagedDrawer />
+                <ToastContainer autoClose={2000} theme="colored" />
               </>  
-              <ManagedModal />
-              <ManagedDrawer />
             </CartProvider>
           </ModalProvider>
         </SearchProvider>
