68acc700c769744327731322b5f6fb7159e1f600
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-08-07 17:54:15 +0100

    chore: adjusted sides for shops display

--- src/components/shops/select-country.tsx
+++ src/components/shops/select-country.tsx
@@ -40,7 +40,7 @@ export default function SelectCountry({
     <div className="">
       <Combobox value={selectedContry} onChange={setSelectedContry}>
         <div className="relative mt-1 z-10">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg border border-accent bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <div className="relative w-full cursor-default overflow-hidden border border-accent bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
               displayValue={(country: string) => country}
